606d140223a5cd05ca41e5486384b0e949c651fb
Bermar Balibalos <bermar@avail.at>
2019-05-22 18:31:30 +0800

    Update layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <title>Home | MYPAYRENT</title>
     <link href="{{ url('/') }}/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ url('/') }}/css/font-awesome.min.css" rel="stylesheet">
