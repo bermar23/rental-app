e63edfb800fae05aca807e1d1e9c85141c5686aa
Bermar Balibalos <bermar@avail.at>
2019-06-04 16:18:05 +0800

    Update footer

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -7,7 +7,7 @@
 				<div class="col-sm-2">
 					<div class="companyinfo">
 						<h2><span>pay</span>rental</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor</p>
+						
 					</div>
 				</div>
 				<div class="col-sm-7">
@@ -28,7 +28,6 @@
 						<h2>Service</h2>
 						<ul class="nav nav-pills nav-stacked">
 							<li><a href="{{ url('contacts-us') }}">Contact Us</a></li>
-							<li><a href="{{ url('orders') }}">Order Status</a></li>
 						</ul>
 					</div>
 				</div>
@@ -55,16 +54,6 @@
 						<ul class="nav nav-pills nav-stacked">
 							<li><a href="#">Company Information</a></li>
 						</ul>
-					</div>
-				</div>
-				<div class="col-sm-3 col-sm-offset-1">
-					<div class="single-widget">
-						<h2>About <span>PAY</span>RENTAL</h2>
-						<form action="#" class="searchform">
-							<input type="text" placeholder="Your email address" />
-							<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
-							<p>Get the most recent updates from <br />our site and be updated your self...</p>
-						</form>
 					</div>
 				</div>
 
