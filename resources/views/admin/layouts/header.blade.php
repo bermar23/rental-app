ab85668f35bfda39d0d59468107292abd89124d4
Bermar Balibalos <bermar@avail.at>
2019-06-04 19:12:49 +0800

    Add main models and views for admin

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -8,16 +8,16 @@
 				<div class="col-sm-6">
 					<div class="contactinfo">
 						<ul class="nav nav-pills">
-							<!--<li><a href="#"><i class="fa fa-phone"></i> MOBILE</a></li>
-							<li><a href="mailto:EMAIL"><i class="fa fa-envelope"></i> EMAIL</a></li>-->
+							<li><a href="#"><i class="fa fa-phone"></i> MOBILE</a></li>
+							<li><a href="mailto:EMAIL"><i class="fa fa-envelope"></i> EMAIL</a></li><!---->
 						</ul>
 					</div>
 				</div>
 				<div class="col-sm-6">
 					<div class="social-icons pull-right">
 						<ul class="nav navbar-nav">
-							<!--<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<!--<li><a href="#"><i class="fa fa-twitter"></i></a></li>
 							<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
 							<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
 							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>-->
@@ -39,9 +39,9 @@
 				<div class="col-md-8 clearfix">
 					<div class="shop-menu clearfix pull-right">
 						<ul class="nav navbar-nav">
-							<!--<li><a href=""><i class="fa fa-user"></i> Account</a></li>
+							<li><a href=""><i class="fa fa-user"></i> Account</a></li>
 							<li><a href="{{ url('login') }}"><i class="fa fa-lock"></i> Login</a></li>
-							<li><a href="{{ url('register') }}"><i class="fa fa-lock"></i> Register</a></li>-->
+							<li><a href="{{ url('register') }}"><i class="fa fa-lock"></i> Register</a></li><!---->
 						</ul>
 					</div>
 				</div>
@@ -64,7 +64,11 @@
 					<div class="mainmenu pull-left">
 						<ul class="nav navbar-nav collapse navbar-collapse">
 							<li><a href="{{ url('/') }}/admin/home" class="active">Home</a></li>
-							<li><a href="{{ url('/') }}/admin/products">Products</a></li>
+							<li><a href="{{ url('/') }}/admin/users">Users</a></li>
+							<li><a href="{{ url('/') }}/admin/items">Products</a></li>
+							<li><a href="{{ url('/') }}/admin/categories">Categories</a></li>
+							<li><a href="{{ url('/') }}/admin/transactions">Transactions</a></li>
+							<li><a href="{{ url('/') }}/admin/invoices">Invoices</a></li>
 						</ul>
 					</div>
 				</div>
