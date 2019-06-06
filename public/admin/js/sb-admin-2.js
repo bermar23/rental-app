c9041849c9083c80058bf03afcc1bb45f5946d62
Bermar Balibalos <bermar@avail.at>
2019-06-06 17:32:01 +0800

    Add sbadmin

---
+++ public/admin/js/sb-admin-2.js
@@ -0,0 +1,173 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/admin-js/sb-admin-2.js":
+/*!******************************************!*\
+  !*** ./resources/admin-js/sb-admin-2.js ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+(function ($) {
+  "use strict"; // Start of use strict
+  // Toggle the side navigation
+
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    }
+
+    ;
+  }); // Close any open menu accordions when window is resized below 768px
+
+  $(window).resize(function () {
+    if ($(window).width() < 768) {
+      $('.sidebar .collapse').collapse('hide');
+    }
+
+    ;
+  }); // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
+    if ($(window).width() > 768) {
+      var e0 = e.originalEvent,
+          delta = e0.wheelDelta || -e0.detail;
+      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+      e.preventDefault();
+    }
+  }); // Scroll to top button appear
+
+  $(document).on('scroll', function () {
+    var scrollDistance = $(this).scrollTop();
+
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  }); // Smooth scrolling using jQuery easing
+
+  $(document).on('click', 'a.scroll-to-top', function (e) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1000, 'easeInOutExpo');
+    e.preventDefault();
+  });
+})(jQuery); // End of use strict
+
+/***/ }),
+
+/***/ "./resources/admin-sass/app.scss":
+/*!***************************************!*\
+  !*** ./resources/admin-sass/app.scss ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/lib/loader.js):\n\r\nundefined\r\n       ^\r\n      Can't find stylesheet to import.\n  ╷\n5 │ @import \"../vendor/bootstrap/scss/bootstrap.scss\";\n  │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  ╵\n  stdin 5:9  root stylesheet\r\n      in D:\\Code\\Personal\\rental.local\\resources\\admin-sass\\app.scss (line 5, column 9)\n    at runLoaders (D:\\Code\\Personal\\rental.local\\node_modules\\webpack\\lib\\NormalModule.js:302:20)\n    at D:\\Code\\Personal\\rental.local\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at D:\\Code\\Personal\\rental.local\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (D:\\Code\\Personal\\rental.local\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at render (D:\\Code\\Personal\\rental.local\\node_modules\\sass-loader\\lib\\loader.js:52:13)\n    at Function.$2 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:23927:48)\n    at xS.$2 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:14932:15)\n    at vX.wa (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8897:42)\n    at vX.w9 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8899:32)\n    at iM.vm (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8296:46)\n    at vj.$0 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8432:7)\n    at Object.eP (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:1465:80)\n    at ak.bc (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8354:3)\n    at j_.bc (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8291:25)\n    at j_.cA (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8278:6)\n    at pL.cA (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8080:35)\n    at Object.m (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:1348:19)\n    at D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:4974:51\n    at yi.a (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:1356:71)\n    at yi.$2 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8095:23)\n    at wU.$2 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8090:25)\n    at vX.wa (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8897:42)\n    at vX.w9 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8899:32)\n    at iM.vm (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8296:46)\n    at vj.$0 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8432:7)\n    at Object.eP (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:1465:80)\n    at ak.bc (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8354:3)\n    at j_.bc (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8291:25)\n    at j_.cA (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8278:6)\n    at Object.eval (eval at DS (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:628:8), <anonymous>:2:37)\n    at vX.wa (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8897:42)\n    at vX.w9 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8899:32)\n    at iM.vm (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8296:46)\n    at vj.$0 (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8432:7)\n    at Object.eP (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:1465:80)\n    at ak.bc (D:\\Code\\Personal\\rental.local\\node_modules\\sass\\sass.dart.js:8354:3)");
+
+/***/ }),
+
+/***/ 0:
+/*!********************************************************************************!*\
+  !*** multi ./resources/admin-js/sb-admin-2.js ./resources/admin-sass/app.scss ***!
+  \********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! D:\Code\Personal\rental.local\resources\admin-js\sb-admin-2.js */"./resources/admin-js/sb-admin-2.js");
+module.exports = __webpack_require__(/*! D:\Code\Personal\rental.local\resources\admin-sass\app.scss */"./resources/admin-sass/app.scss");
+
+
+/***/ })
+
+/******/ });
